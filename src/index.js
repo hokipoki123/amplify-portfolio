a281966e144eab36f32db7243924c10c098553c4
Allie Shirer <jackshirer96@gmail.com>
2022-12-24 17:45:57 +1300

    instal redux and store

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ import reportWebVitals from './reportWebVitals'
 import { Amplify } from 'aws-amplify'
 import awsExports from './aws-exports'
 import '@aws-amplify/ui-react/styles.css'
+import { Provider } from 'react-redux'
+import store from './store'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 Amplify.configure(awsExports)
 
@@ -20,12 +23,23 @@ Amplify.configure(awsExports)
 //   )
 // }
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>,
+    document.getElementById('app')
+  )
+})
+
+// const root = ReactDOM.createRoot(document.getElementById('root'))
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// )
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
