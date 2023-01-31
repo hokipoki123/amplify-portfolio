3946bd26fdba853bf2e20c1d16e861c6927a2b5e
ashirer <ashirer@allie-win-desktop>
2023-01-31 18:56:40 +1300

    minor language tweak

--- src/Home.jsx
+++ src/Home.jsx
@@ -5,7 +5,7 @@ class Home extends Component {
     return (
       <div>
         <h2>Who am I?</h2>
-        <p>Hi, I'm Allie. I'm a recent graduate of Dev Academy Aotearoa who's keen to enter the workforce. My Portfolio here whilst not the most skilled is an example of something I have done from scratch. This website uses React.js, AWS, Javascript, Node.js, CSS and more.</p>
+        <p>Hi, I'm Allie. I'm a recent graduate of Dev Academy Aotearoa who's keen to enter the workforce. My Portfolio here is an example of something I have done from scratch. This website uses React.js, AWS, Javascript, Node.js, CSS and more.</p>
         <p>Dev Academy Aotearoa is a very intensive course that is designed to bring candidates with no experience and output junior Full Stack Developers. The Course mostly covers Javascript, React and Redux but many other tools are incorporated into the learning. It is also based on teamworking as this is how code is done in the industry. The course is 40-60 hours a week so it also exposed us to a lot of what programming is like, although of course, we were beginners in skill.</p>
         <p>I am very excited to get into the workforce and start to consolidate the skills I have learned so far. As may be noticeable I am not particularly focused on the CSS styling side of web development. Because I like history I decided to make my website slightly resemble a fired tablet from ancient Egypt. Part of the reason I went for this style is because scribes were in high demand and I think programming these days resembles scribes of Egypt.</p>
       </div>
